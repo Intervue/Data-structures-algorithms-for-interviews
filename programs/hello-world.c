71b9c586877be88f64a02bdb5939b52ace6e563f
arorarahul <arorar1992@gmail.com>
2017-02-02 12:30:14 +0530

    updated file

--- programs/hello-world.c
+++ programs/hello-world.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
+//pre-process fetching contents of library stdio.h which contains predefined functions in C
 
-int main(){
-	printf("Hello world \n");
+int main(){//default function that is run by C everytime
+	printf("Hello world \n"); //predefined function from the library which prints the output given to it
 }
