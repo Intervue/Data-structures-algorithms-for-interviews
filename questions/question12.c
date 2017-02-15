a53276232ba7b668a5b685ae21869d0983b629c6
arorarahul <arorar1992@gmail.com>
2017-02-15 13:32:06 +0530

    alternate way added

--- questions/question12.c
+++ questions/question12.c
@@ -40,6 +40,21 @@ int main(){
 	
 }
  
+//Alternate way
+// #include <stdio.h>
+// int main(){
+//     int n1, n2;
+//     printf("Enter two positive integers: ");
+//     scanf("%d %d",&n1,&n2);
+//     while(n1!=n2){
+//         if(n1 > n2)
+//             n1 -= n2;
+//         else
+//             n2 -= n1;
+//     }
+//     printf("GCD = %d",n1);
+//     return 0;
+// } 
 
 
 
