5cb087f0656b18a56acafe2cb2dc46db87cf2e18
arorarahul <arorar1992@gmail.com>
2017-02-15 13:30:38 +0530

    another question added

---
+++ questions/question12.c
@@ -0,0 +1,48 @@
+/*
+2. Write a C Program to find the Greatest Common Divisor (GCD) of two positive integers,
+   using a function "int GCD (int a, int b)" which returns the GCD of two
+   positive integers "a" and "b" passed as arguments. Note that the values of "a" and "b" might be
+   arbitrary, and proper arrangements should be made to ensure that the
+   function works for both the cases: "a >= b" and "a < b". 
+   Input : two positive integers in the "main()" portion of the program.
+   Output: the GCD, as returned by the "GCD()" function.    [2]
+*/
+#include <stdio.h>
+
+int GCD(int a, int b){
+
+   int min, value;
+   min = (a < b)?a:b;
+
+   if(a%min == 0 && b%min == 0){
+      value = min;
+   }else{
+      for(int i= (min-1); i>1;i--){
+         if(a%i ==0 && b%i ==0){
+            value = i;
+            break;
+         }
+      }
+   }
+   return value;
+}
+
+int main(){
+
+   int num1, num2, ans;
+
+   printf("Enter two positive integers:\n");
+   scanf("%i %i", &num1, &num2);
+   
+   ans = GCD(num1, num2);
+
+   printf("GCD for %d and %d is %d\n",num1, num2, ans);
+	
+}
+ 
+
+
+
+
+
+
