477a418753bb073c97dd76fe3c22208d86fa1741
arorarahul <arorar1992@gmail.com>
2017-02-21 00:38:19 +0530

    merge sort updated

--- arrays/question3.c
+++ arrays/question3.c
@@ -11,53 +11,56 @@ Therefore: O(nlogn)
 #include <stdio.h>
 #include <math.h>
 
-void print_array(int arr[], int n){
-	printf("value of n %d\n",n);
-	for(int i=0;i<n;i++){
-		printf("%d ", arr[i]);
-	}
-}
-
 void merge(int arr[],int p, int q, int r){
-	long unsigned int f = INFINITY;
-	int n1,n2,i,j;
-	n1 = q-p+1;
-	n2 = r-q;
-
-	int left[n1+1],right[n2+1];
-	for(i=0; i<n1;i++){
-		left[i]=arr[i];
-	}
-	for(j=0; j<n2;j++){
-		right[j]=arr[q+j+1];
-	}
-	left[n1]=right[n2]=f;
-	i=j=0;
-	for(int k=0; k<(r-p+1); k++){
-		if(left[i]<right[j]){
-			arr[k]=left[i];
-			i++;
-		}else{
-			arr[k]=right[j];
-			j++;
-		}
-	}
-	print_array(arr,(r-p+1));
+  long unsigned int f = INFINITY;
+  int n1= q-p+1;
+  int n2 = r-q;
+  int left[n1+1], right[n2+1];
+  int i,j,k;
+  
+  for(i=0;i<n1;i++){
+    left[i]=arr[p+i];
+    printf("%d ", left[i]);
+  }
+  
+  for(j=0;j<n2;j++){
+    right[j]=arr[q+j+1];
+    printf("%d ", right[j]);
+  } 
+  
+  left[n1] = right[n2] = f;
+  i=j=0;
+  for(k=p;k<(r+1);k++){
+    if(left[i] < right[j]){
+      arr[k] = left[i];
+      i++;
+    }else{
+      arr[k] = right[j];
+      j++;
+    }
+  }
+  
+  for(int z=0; z<(r+1);z++){
+    printf("%d ", arr[z]);
+  }
+  
 }
 
-void merge_sort(int arr[],int p,int r){
-	if(p<r){
-		int q = (p+r)/2;
-		merge_sort(arr,p,q);
-		merge_sort(arr,q+1,r);
-		merge(arr,p,q,r);
-	}
+void merge_sort(int arr[], int p, int r){
+  
+  int q;
+  if(p<r){
+    q = (p+r)/2;
+    merge_sort(arr,p,q);
+    merge_sort(arr,q+1,r);
+    merge(arr,p,q,r);
+  }
 }
 
 int main(){
-	int a[6] = {5,6,9,0,2,8};
-	int p = 1;
-	int size = 6;
-	int q = (p+size)/2;
-	merge_sort(a,p,size);
+  
+  int arr[] = {9,6,5,0,8,2};
+  int length = sizeof(arr)/sizeof(arr[0]);
+  merge_sort(arr,0,length-1);
+
 }
---
+++ arrays/question4.c
@@ -0,0 +1,11 @@
+/*
+PARTITION PROCEDURE
+
+Time complexity: O(n)
+Space complexity: O(n)
+
+*/
+#include <stdio.h>
+#include <math.h>
+
+
