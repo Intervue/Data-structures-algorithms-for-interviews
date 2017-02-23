f5ba63ae42f9f47f4e5a44a430b19935e49d2c64
arorarahul <arorar1992@gmail.com>
2017-02-23 21:09:50 +0530

    hash table method included

--- arrays/question8.c
+++ arrays/question8.c
@@ -1,22 +1,72 @@
 /*
-RADIX SORT
-numbers to be sorted should be in a range eg (1-5) or (m to m+k)
-
-
-Time complexity
-O(n+k) k = range of numbers n= number of elemnts in input 
-
-Space complexity = size of DS
-O(k)
+find a pair in an array whose sum is equal to a given number
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-
+/*METHOD1: by iterating and find suitable pairs
+Time complexity: O(n^2) time
+*/
 
 int main(){
-	int a[] = {1,1,4,4,2,5,6,8,3};
 	
+	int a[] = {2,5,8,1,4,5};
+	int j;
+
+	printf("pairs whose sum is 6 are\n");
+
+	for(int i=0; i<6;i++){
+		int key1 = a[i];
+		
+		for(j=i+1;j<6;j++){
+			int key2 = a[j];
+			
+			if(key1+key2 == 6){
+				printf("%d and %d ",key1,key2);
+				printf("\n");
+			}	
+
+		}
+	}
+
 }
+/*METHOD2: hash table
+Insert all numbers into a hash table. O(n)
+For every element a, search b in hash table such that sum is x O(1)
+
+Time complexity: O(n) time
+Space complexity: size of hash O(n) time
+*/
+
+#include <stdio.h>
+#define MAX 10
+
+void findPairs(int arr[],int size,int sum){
+
+	int index,temp;
+	int hash[MAX] = {0};
+
+	for(index = 0; index<size; index++){
+
+		temp = sum - arr[index];
+		if(temp>=0 && hash[temp] == 1){
+			printf("Pair with the given sum %d is %d and %d ", sum,temp,arr[index]);
+		}
+		hash[arr[index]]=1;
+	}
+
+}
+
+int main(){
+	int a[] = {2,5,8,1,4,5};
+
+	int length = sizeof(a)/sizeof(a[0]);
+
+	int sum = 9;
+
+	findPairs(a,length, sum);
+
+}
+
