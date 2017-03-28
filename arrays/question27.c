6137a6a0d53b6ab77c83ed2eeb6260ef238714d4
arorarahul <arorar1992@gmail.com>
2017-03-28 15:39:51 +0530

    new question added

--- arrays/question27.c
+++ arrays/question27.c
@@ -21,3 +21,68 @@ Time complexity: O(n^2)
 Space complexity: O(1)
 */
 
+//METHOD1
+#include <stdio.h>
+#include <stdlib.h>
+
+int findTriangles(int arr[], int size){
+	int total = 0;
+	
+	for(int i = 0; i<size-2; i++){
+		int a = arr[i];
+		for(int j=i+1;j<size-1;j++){
+			int b = arr[j];
+			for(int k=j+1; k<size;k++){
+				int c = arr[k];
+				if((a+b>c) && (b+c>a) && (c+a>b)){
+					total++;
+				}
+			}
+		}
+	}
+
+	return total;
+}
+
+int main(){
+	int a[] = {1,3,2,10};
+	int size = sizeof(a)/sizeof(a[0]);
+
+	printf("The number of triangles that can be formed are : %d\n", findTriangles(a, size));
+
+	return 0;
+}
+
+//METHOD2
+#include <stdio.h>
+#include <stdlib.h>
+
+int findTriangles(int arr[], int size){
+	int total = 0;
+	int k;
+	for(int i=0; i<size-2;i++){
+		for(int j=i+1;j<size-1;j++){
+			k = j+1;
+			if(arr[i] + arr[j] > arr[k]){
+				total++;
+			}
+		}
+	}
+	return total;
+	
+}
+
+int compar(const void*a, const void *b){
+	return (*(int*)a-*(int*)b);
+}
+
+int main(){
+	int a[] = {4,1,3,2};
+	int size = sizeof(a)/sizeof(a[0]);
+	qsort(a,size,sizeof(int),compar);
+
+	printf("The number of triangles that can be formed are : %d\n", findTriangles(a, size));
+
+	return 0;
+}
+
