e05d1371b2be4b3e58243a1c177345fd2fc8061c
arorarahul <arorar1992@gmail.com>
2017-05-13 14:41:30 +0530

    new question added

--- trees/question8.c
+++ trees/question8.c
@@ -10,3 +10,58 @@ variable for diameter which can be updated if the new computed values is greater
 Time complexity: O(n)
 Space complexity: O(n)
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+	int data;
+	struct node *left;
+	struct node *right;
+};
+
+int max_dia = 0;
+
+struct node *newNode(int data){
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	temp->data = data;
+	temp->left = temp->right= NULL;
+	return temp;
+}
+
+int max(int left, int right){
+	return (left < right)?right: left;
+}
+
+int findDiameter(struct node *root){
+	if(!root){
+		return 0;
+	}
+	int left = findDiameter(root->left);
+	int right = findDiameter(root->right);
+	int dia = left+right;
+	if(max_dia < dia){
+		max_dia = dia;
+	}
+	return 1 + max(left,right);
+}
+
+int main(){
+	struct node *root = NULL;
+	root = newNode(2);
+	root->left = newNode(3);
+	root->right = newNode(4);
+	root->left->right = newNode(5);
+	root->left->right->right = newNode(6);
+	root->left->right->right->right = newNode(7);
+	root->left->left = newNode(8);
+	root->left->left->right = newNode(10);
+	root->left->left->left = newNode(9);
+
+	findDiameter(root);
+	printf("max dia is...%d\n", max_dia);
+	return 0;
+}
+
+
+
