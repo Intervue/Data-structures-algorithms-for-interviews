612cf00b2594c094b322f2fd629de7e7b5e73609
arorarahul <arorar1992@gmail.com>
2017-06-22 18:49:15 +0530

    new question added

--- dynamic-programming/question21.c
+++ dynamic-programming/question21.c
@@ -43,6 +43,82 @@ if    1    n-1  //if value of i is 1 j can be between 1 and n-1, therefore it ca
 
       That means there are n^2 unique sub problems
 
-Therefore, we make a matrix of size n^2 and ...
+Therefore, we make a matrix of size n^2 and taking example as "naudna"
+There is a default row 0 for the matrix and apart from that matrix will look like this
 
+   0 n a u d n a
+0
+n
+a
+u
+d
+n
+a
+
+The cells below the diagnal element will not be considered as i always needs to be less than j. Now we traverse
+The matrix diagnally each time and as per recursive equations keep filling the matrix.
+Note that the diagnal element will initially be all 1 as n a u d n a are all palindromes if taken one at a time
+
+By repeating this process we can actually find a palindrome with the longest length.
+
+Time complexity: O(n^2)
+Space complexity: O(n^2)
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int findMax(int a, int b){
+	return (a>b)?a:b;
+}
+
+int findLongestPal(char *str){
+
+	int length = strlen(str);
+	int dp[length][length];
+	int i,j;
+
+	for(i=0;i<length;i++){
+		dp[i][i] = 1;
+	}
+	int k;
+	for(j=1;j<length;j++){
+		i=0,k=j;
+		while(k<=5){
+			if(str[i] == str[k]){
+				dp[i][k] = 2 + dp[i+1][k-1];
+			}else{
+				dp[i][k] = findMax(dp[i+1][k],dp[i][k-1]);
+			}
+			k++;
+			i++;	
+		}	
+	}
+	return dp[0][length-1];
+}
+
+int main(){
+	char str[] = "raudra";
+
+	int len = findLongestPal(str);
+	printf("longest palindrome is with length %d\n", len);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
